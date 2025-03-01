66b9abc6c8f5066ce706ab1386ffa4d23d8413dc
TheBatOfGotham <joshuajk5328@gmail.com>
2025-03-01 12:33:24 +0530

    Added append, loading.

--- frontend/src/Pages/Dash.jsx
+++ frontend/src/Pages/Dash.jsx
@@ -1,24 +1,52 @@
 import "../styles/Dash.css";
-import {useContext} from "react";
-import {BrowserRouter as Router, Routes, Route, useNavigate} from "react-router-dom";
-import Heading from "../components/Heading.jsx"
+import { useContext } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+} from "react-router-dom";
+import Heading from "../components/Heading.jsx";
 import { BrandNameContext } from "../contexts/BrandNameContext.jsx";
 
 function Dash() {
-	const navigate = useNavigate();
-	const { brandName } = useContext(BrandNameContext);
+  const navigate = useNavigate();
 
-	console.log('Dash Page Brand Name:', brandName);
+  const handleClick = () => {
+    navigate("/dash");
+  };
+
+  const handleDoubleClick = () => {
+    navigate("/home");
+  };
+
+  const { brandName } = useContext(BrandNameContext);
+
+  console.log("Dash Page Brand Name:", brandName);
 
   return (
-		<div className="dashboard">
-			<Heading name={brandName}/>
-			<div className="accessibility">
-				<button className="thebutton new" onClick={()=> navigate("/add-new")}>Add New Stock</button>
-				<button className="thebutton return" onClick={()=> navigate("/add-returned")}>Add Return/Sales</button>
-				<button className="thebutton view" onClick={()=> navigate("/view")}>View Saved Stock</button>
-			</div>
-		</div>
+    <div className="dashboard">
+      <Heading
+        name={brandName}
+        onClick={handleClick}
+        onDoubleClick={handleDoubleClick}
+      />
+
+      <div className="accessibility">
+        <button className="thebutton new" onClick={() => navigate("/add-new")}>
+          Add New Stock
+        </button>
+        <button
+          className="thebutton return"
+          onClick={() => navigate("/add-returned")}
+        >
+          Add Return/Sales
+        </button>
+        <button className="thebutton view" onClick={() => navigate("/view")}>
+          View Saved Stock
+        </button>
+      </div>
+    </div>
   );
 }
 
---
+++ frontend/src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Loading.css';
+
+const Loading = () => {
+    return (
+        <div className="wrapper">
+            <div className="circle"></div>
+        </div>
+    );
+};
+
+export default Loading;
